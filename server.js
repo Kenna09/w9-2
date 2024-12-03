af22803f3f18ffaa14a9a0bddfdc5375daeb653a
Kenna09 <kennagarcia09@gmail.com>
2024-12-03 14:38:03 -0800

    Update server.js port

--- server.js
+++ server.js
@@ -83,7 +83,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const app = express();
-const port = 3000;
+const port = 8081;
 
 // Configure the PostgreSQL client
 const pool = new Pool({
